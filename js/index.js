22e34f1cfd2c64b181d9343d0ca5c470df1ef270
matheusfell <bentomatheus48@gmail.com>
2023-05-16 00:32:17 -0300

    add scroll suave

--- js/index.js
+++ js/index.js
@@ -1,12 +1,4 @@
-const menuItems = document.querySelectorAll('.header-menu a');
-
-menuItems.forEach(item => {
-  item.addEventListener('click', scrollToIdOnClick)
+window.scroll({
+	top: 0,
+	behavior: 'smooth',
 })
-
-function scrollToIdOnClick(event) {
-  const element  = event.target;
-  const id = element.getAtribute('href');
-  const section = document.querySelector(id);
-  console.log(section.offset);
-}
